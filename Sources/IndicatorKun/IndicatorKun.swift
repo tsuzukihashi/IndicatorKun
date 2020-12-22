5f0597f8f8d58a460b25001356398c44fbc24881
tsuzukihashi <tsudukihashi0817@gmail.com>
2020-12-23 00:06:53 +0900

    複数のパラメータを外部から渡せるように修正

--- Sources/IndicatorKun/IndicatorKun.swift
+++ Sources/IndicatorKun/IndicatorKun.swift
@@ -7,42 +7,61 @@ import SwiftUI
 public struct IndicatorKun: View {
     @State var isAnimation = false
     let title: String
+    let backgroundColor: Color
+    let backSize: CGSize
+    let circleColors: [Color]
+    let circleSize: CGSize
+    let font: Font
     
-    public init(title: String = "読み込み中") {
+    public init(
+        title: String = "読み込み中",
+        backgroundColor: Color = .gray,
+        backSize: CGSize = .init(width: 200, height: 120),
+        circleColors: [Color] = [.gray, .white],
+        circleSize: CGSize = .init(width: 48, height: 48),
+        font: Font = .system(size: 12, weight: .medium, design: .rounded)
+    ) {
         self.title = title
+        self.backgroundColor = backgroundColor
+        self.backSize = backSize
+        self.circleColors = circleColors
+        self.circleSize = circleSize
+        self.font = font
     }
     
     public var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 8)
-                .frame(width: 200, height: 120, alignment: .center)
-                .foregroundColor(Color.gray)
-
+                .frame(width: backSize.width, height: backSize.height, alignment: .center)
+                .foregroundColor(backgroundColor)
+            
             VStack {
                 Spacer()
                 Circle()
                     .trim(from: 0, to: 0.6)
-                    .stroke(AngularGradient(gradient: Gradient(colors: [.gray, .white]), center: .center),
-                            style: StrokeStyle(
-                                lineWidth: 8,
-                                lineCap: .round,
-                                dash: [0.1, 16],
-                                dashPhase: 8))
-
-                    .frame(width: 48, height: 48)
+                    .stroke(
+                        AngularGradient(gradient: Gradient(colors: circleColors), center: .center),
+                        style: StrokeStyle(
+                            lineWidth: 8,
+                            lineCap: .round,
+                            dash: [0.1, 16],
+                            dashPhase: 8
+                        )
+                    )
+                    .frame(width: circleSize.width, height: circleSize.height)
                     .rotationEffect(Angle(degrees: self.isAnimation ? 360 : 0))
                     .onAppear() {
                         withAnimation(
                             Animation
                                 .linear(duration: 1)
                                 .repeatForever(autoreverses: false)) {
-                                    self.isAnimation.toggle()
+                            self.isAnimation.toggle()
                         }
-                }
-
-                Text("読み込み中")
+                    }
+                
+                Text(title)
                     .foregroundColor(.white)
-                    .font(.system(size: 12, weight: .medium, design: .rounded))
+                    .font(font)
                     .lineLimit(1)
                     .padding(.top)
                 Spacer()
